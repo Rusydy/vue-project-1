702bce120b297af27a28da7771ceba9506990d0d
Rusydy <rusydymuhiddin@gmail.com>
2024-07-15 00:23:36 +0700

    chore: Update App.vue to display a to-do list and add new task functionality

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,51 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <h1>
+    {{ title }}
+  </h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <ul>
+    <li v-for="task in tasks" :key="task.id">
+      {{ task.id }}. {{ task.name }}
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+      <div v-if="task.finished">
+        <button @click="task.finished = false">Delete Task</button>
+      </div>
+      <div v-else-if="task.edit">
+        <button @click="task.edit = false">Edit</button>
+      </div>
+      <div v-else>
+        <p>No button</p>
+      </div>
+    </li>
+  </ul>
 
-  <RouterView />
+  <h2>
+    Add a new task
+  </h2>
+
+  <div v-if="newTask.length > 0">
+    <h3>
+      New task preview
+    </h3>
+
+    <p>
+      {{ newTask }}
+    </p>
+
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+<script>
+export default {
+  data() {
+    return {
+      title: 'My To Do App',
+      tasks: [
+        { id: 1, name: 'Learn Vue', finished: false },
+        { id: 2, name: 'Learn Vuex', finished: false },
+        { id: 3, name: 'Learn Vue Router', finished: false }
+      ]
+    }
   }
 }
-</style>
+</script>
